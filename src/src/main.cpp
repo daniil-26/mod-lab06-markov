70d3cf189dc59e70c990c86f6804d820afbd0786
daniil-26 <126852384+daniil-26@users.noreply.github.com>
2023-04-23 16:34:29 +0300

    Create main.cpp

---
+++ src/src/main.cpp
@@ -0,0 +1,22 @@
+// Copyright 2021 GHA Test Team
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <deque>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include "textgen.h"
+
+
+typedef std::deque<std::string> prefix;
+typedef std::map<prefix, std::vector<std::string>> tab;
+
+
+int main() {
+    const int NPREF = 2;
+    const int MAXGEN = 1000;
+
+    tab statetab = create_tab("text.txt", NPREF);
+    generate("generation_example.txt", statetab, MAXGEN, NPREF);
+}

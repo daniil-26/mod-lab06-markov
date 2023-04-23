85171bda8c88109292944c1376c48e47bbfd02d8
daniil-26 <126852384+daniil-26@users.noreply.github.com>
2023-04-23 16:40:14 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -13,6 +13,14 @@ typedef std::deque<std::string> prefix;
 typedef std::map<prefix, std::vector<std::string>> tab;
 
 
+template <typename T>
+T random_element(std::vector<T> v);
+
+prefix create_prefix(std::ifstream& file, int npref);
+
+void create_note(tab* statetab, prefix key, std::string word);
+
+
 tab create_tab(std::string fname, int npref);
 
 void generate(std::string fname, tab statetab, int maxgen, int npref);

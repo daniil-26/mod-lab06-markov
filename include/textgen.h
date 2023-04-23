694bfdaaa3189833aef89d58631bf5481ceda767
daniil-26 <126852384+daniil-26@users.noreply.github.com>
2023-04-23 15:46:03 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -1,3 +1,4 @@
+// Copyright 2021 GHA Test Team
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -7,12 +8,11 @@
 #include <algorithm>
 #include <random>
 
-using namespace std;
 
-typedef deque<string> prefix;
-typedef map<prefix, vector<string>> tab;
+typedef std::deque<std::string> prefix;
+typedef std::map<prefix, std::vector<std::string>> tab;
 
 
-tab create_tab(string fname, int npref);
+tab create_tab(std::string fname, const int npref);
 
-void generate(string fname, const tab statetab, const int maxgen, const int npref);
+void generate(std::string fname, tab statetab, const int maxgen, const int npref);
